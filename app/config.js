4761227dd6b593641a27d8e2a7cb3778eca08b54
Ilya A. Shlyakhovoy <bulgarus@inbox.ru>
2019-10-02 19:24:56 +0300

    done

--- app/config.js
+++ app/config.js
@@ -1,10 +1,14 @@
 module.exports = {
     workers: 5,
-    cycle: 3000,
-    lag: 100,
+    cycle: 2000,
+    delay: 6000,
+    lag: 1000,
     timeout: 10000,
     channel: 'channel',
     signal: 'signal_bus',
     errors: 'error_bus',
-    state: 'state_value'
+    generator: 'generator',
+    nextGenerator: 'next_generator',
+    state: 'state_value',
+    lastReceived: 'last_received'
 }
--- app/service.js
+++ app/service.js
@@ -7,75 +7,96 @@ const config = require('./config');
 bluebird.promisifyAll(redis);
 
 class Processor {
-    /*     thread = undefined;
-        isGenerator = false;
-        client = undefined;
-        lastMessage = 0;
-        channel = undefined;
-        errors = undefined;
-        state = undefined;
-        signal = undefined;
-        lag = 0; */
-    constructor(config, workerData) {
+    constructor(workerData) {
         this.thread = workerData.thread;
-        this.channel = config.channel;
-        this.errors = config.errors;
-        this.state = config.state;
-        this.signal = config.signal;
-        this.lag = config.lag;
-        this.client = redis.createClient({
-            host: 'redis',
-            retry_strategy: function (options) {
-                return Math.max(options.attempt * 100, config.timeout - config.lag);
-            }
-        });
-
-        this.client.on("error", function (err) {
-            console.log(`Error in thread ${workerData.thread}: `, err);
-        });
-
-        this.client.on("message", function (channel, message) {
-            console.log('message', this.thread, channel, message)
-            if (channel !== this.channel) return;
-
-            /*  const num = Number(message);
-             if (isNaN(num) || num > 8) {
-                 client.lpushAsync(this.errors, {
-                     thread: this.thread,
-                     error: isNaN(num) ? 'not a number' : 'number too big',
-                     time: Date.now()
-                 })
-                     .catch(err => console.log(err));
-                 console.log(isNaN(num) ? 'not a number' : 'number too big', num)
-                 return;
-             }
-             this.client.lpopAsync(this.state, function (err, result) {
-                 if (err || !result) {
-                     this.client.setAsync(this.state, 1)
-                         .catch(err => console.log(err));
-                 } else {
-                     this.client.incrAsync(this.state)
-                         .catch(err => console.log(err));
-                 }*/
-        })
+        this.pubClient = this.createClient(config.timeout - config.lag);
+        this.subClient = this.createClient(config.timeout - config.lag);
     }
 
+    createClient(maxTimeout) {
+        const client = redis.createClient({
+            host: 'redis',
+            retry_strategy: function (options) {
+                return Math.max(options.attempt * 100, maxTimeout);
+            }
+        });
+        client.on("error", function (err) {
+            console.log(`Error in thread ${this.thread}: `, err);
+        });
+        return client;
+    }
 
     async run() {
-        this.client.subscribe(this.channel);
         while (true) {
-            if (this.thread == 0) {
-                console.log('tock')
-                this.client.lpushAsync(this.signal, Math.floor(Math.random() * 11))
+            // if we are waiting for another generator, downgrade to worker
+            const generator = await this.subClient.getAsync(config.generator);
+            const nextGenerator = await this.subClient.getAsync(config.nextGenerator);
+            const receivedMessageTime = await this.subClient.getAsync(config.lastReceived);
+            if ((generator == this.thread && nextGenerator == null) || nextGenerator == this.thread) {
+                await this.pubClient.delAsync(config.nextGenerator);
+                await this.pubClient.setAsync(config.generator, this.thread);
+                // 5% each time for the generator fail
+                if (Math.floor(Math.random() * 10) === 0) {
+                    // go to sleep till timeout - generator was broken
+                    console.log('generator', this.thread, 'broken')
+                    await sleep(config.timeout, config.lag)
+                    continue;
+                }
+                const num = Math.floor(Math.random() * 11);
+                this.pubClient.lpushAsync(config.signal, num)
+                    .then(() => {
+                        console.log('PUSH #', this.thread, num)
+                    })
+                    .catch(e => console.log(`Error in thread ${this.thread}: `, err));
+            } else {
+                this.subClient.lpopAsync(config.signal)
+                    .then(async res => {
+                        // no new data after timeout!                    
+                        if (res === null) {
+                            const now = Date.now()
+                            if ((now - receivedMessageTime) > config.delay) {
+                                console.log('too long waiting #', this.thread)
+                                // make generator working next step
+                                const code = await this.subClient.getAsync(config.nextGenerator);
+                                if (code === null) {
+                                    await this.pubClient.setAsync(config.nextGenerator, this.thread)
+                                }
+                            }
+                            return
+                        }
+                        await this.pubClient.setAsync(config.lastReceived, Date.now());
+                        if (res > 8) {
+                            this.pubClient.lpushAsync(config.errors, JSON.stringify({
+                                thread: this.thread,
+                                error: 'number too big',
+                                time: Date.now()
+                            }))
+                                .catch(err => console.log(err));
+                            console.log('number too big')
+                            return;
+                        };
+                        this.subClient.getAsync(config.state)
+                            .then(() => {
+                                this.pubClient.incrAsync(config.state)
+                                    .then(res => console.log('incremented', res))
+                                    .catch(err => console.log(err))
+                            })
+                            .catch(() => this.pubClient.setAsync(config.state, 1)
+                                .then(res => console.log('incremented', 1))
+                                .catch(err => console.log(err)));
+
+                    })
+                    .catch(err => console.log('pop error', this.thread, err))
+
             }
-            await sleep(1000, 0)
+            await sleep(config.cycle, config.lag)
         }
-        this.client.unsubscribe(this.channel);
     }
 
     close() {
-        this.client.quit();
+        this.pubClient.quit();
+        this.subClient.quit();
     }
 }
 
-new Processor(config, workerData).run();
+new Processor(workerData).run();
